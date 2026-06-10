0657d96b34b06d6ccec59b409649611f4a9706ff
Riya kapadiya <kapadiyariya107@gmail.com>
2026-06-10 11:29:24 +0530

    hello world

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import view
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('view/', view.world),
 ]
